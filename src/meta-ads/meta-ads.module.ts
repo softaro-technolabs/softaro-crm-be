512172465f1c9c983dd33f4ee3d1e2bfe3c5d9b8
satweek <satwek.softarotechnolas.com>
2026-04-03 17:46:09 +0530

    refactor: remove unused EncryptionService and RequestContextService from MetaAdsModule providers

--- src/meta-ads/meta-ads.module.ts
+++ src/meta-ads/meta-ads.module.ts
@@ -2,12 +2,10 @@ import { Module } from '@nestjs/common';
 import { MetaAdsService } from './meta-ads.service';
 import { MetaAdsController, MetaAdsWebhookController } from './meta-ads.controller';
 import { LeadsModule } from '../leads/leads.module';
-import { EncryptionService } from '../common/services/encryption.service';
-import { RequestContextService } from '../common/utils/request-context.service';
 
 @Module({
     imports: [LeadsModule],
-    providers: [MetaAdsService, EncryptionService, RequestContextService],
+    providers: [MetaAdsService],
     controllers: [MetaAdsController, MetaAdsWebhookController],
     exports: [MetaAdsService]
 })
